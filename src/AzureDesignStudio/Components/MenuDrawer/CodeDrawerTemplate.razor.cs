1d8a6c15771afe7de678757a5d021b31f77bd9ad
Victor Miron <victormiron@microsoft.com>
2022-09-21 10:33:54 +0300

    Moved octokit package to server side

--- src/AzureDesignStudio/Components/MenuDrawer/CodeDrawerTemplate.razor.cs
+++ src/AzureDesignStudio/Components/MenuDrawer/CodeDrawerTemplate.razor.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
-using Octokit;
 using System.Text;
 
 namespace AzureDesignStudio.Components.MenuDrawer
